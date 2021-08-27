b0ceee128778c41ecabea7309e2a2831602a3bb3
Leandro Lopes <leandrolopes.help@gmail.com>
2021-08-27 18:24:16 -0300

    Projeto Base

---
+++ mvp-grid/src/main/java/br/com/itau/mvpgrid/MvpGridApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.mvpgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpGridApplication.class, args);
+	}
+
+}
---
+++ mvp-grid/src/test/java/br/com/itau/mvpgrid/MvpGridApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.mvpgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpGridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
